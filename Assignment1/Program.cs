58932162bc7d809d7eb8e6a548ec9611359ce967
SuperTNT17 <r.lemmers@outlook.com>
2021-09-01 18:25:44 +0200

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable 'name' is declared here
+            string name;
+
+            // De gebruiker vragen om hun naam
+            Console.Write("Enter your name: ");
+
+            // read the name of the user and save it in variable 'name'
+            name = Console.ReadLine();
+
+            // De gebruiker vragen om hun leeftijd
+            Console.Write("Enter your age: ");
+
+            // read the age of the user and save it in variable 'age'
+            // (variable 'age' is declared here and immediately given a value)
+            string age = Console.ReadLine();
+
+            // display name and age
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("Your age is " + age);
+
+            // wait for the user to enter a key
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read first name of user
+            Console.Write("Enter your first name: ");
+            string firstname = Console.ReadLine();
+
+            // read last name of user
+            Console.Write("Enter your last name: ");
+            string lastname = Console.ReadLine();
+
+            // display full name (option 1)
+            //Console.WriteLine("Your full name is " + firstname + " " + lastname);
+
+            // display full name (option 2)
+            //Console.WriteLine("Your full name is {0} {1}", firstname, lastname);
+
+            // display full name (option 3 - string interpolation)
+            Console.WriteLine($"Your full name is {firstname} {lastname}");
+
+            //wait for the user to enter a key
+            Console.ReadKey();
+        }
+    }
+}
